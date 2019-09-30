d227e3f743e4f18ec0144e3a5f346b26c61e7f17
Patrick Atkinson <pa13269@my.bristol.ac.uk>
2019-09-30 14:41:13 +0100

    add variable for image width and height

--- stencil.c
+++ stencil.c
@@ -6,13 +6,16 @@
 // Define output file name
 #define OUTPUT_FILE "stencil.pgm"
 
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image);
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image);
-void output_image(const char * file_name, const int nx, const int ny, double *image);
+void stencil(const int nx, const int ny, const int height, double* image,
+             double* tmp_image);
+void init_image(const int nx, const int ny, const int height, double* image,
+                double* tmp_image);
+void output_image(const char* file_name, const int nx, const int ny,
+                  const int height, double* image);
 double wtime(void);
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char* argv[])
+{
   // Check usage
   if (argc != 4) {
     fprintf(stderr, "Usage: %s nx ny niters\n", argv[0]);
@@ -24,55 +27,63 @@ int main(int argc, char *argv[]) {
   int ny = atoi(argv[2]);
   int niters = atoi(argv[3]);
 
+  // we pad the outer edge of the image to avoid out of range address issues in
+  // stencil
+  int width = nx + 2;
+  int height = ny + 2;
+
   // Allocate the image
-  double *image = malloc(sizeof(double)*(nx+2)*(ny+2));
-  double *tmp_image = malloc(sizeof(double)*(nx+2)*(ny+2));
+  double* image = malloc(sizeof(double) * width * height);
+  double* tmp_image = malloc(sizeof(double) * width * height);
 
   // Set the input image
-  init_image(nx, ny, image, tmp_image);
+  init_image(nx, ny, height, image, tmp_image);
 
   // Call the stencil kernel
   double tic = wtime();
   for (int t = 0; t < niters; ++t) {
-    stencil(nx, ny, image, tmp_image);
-    stencil(nx, ny, tmp_image, image);
+    stencil(nx, ny, height, image, tmp_image);
+    stencil(nx, ny, height, tmp_image, image);
   }
   double toc = wtime();
 
-
   // Output
   printf("------------------------------------\n");
-  printf(" runtime: %lf s\n", toc-tic);
+  printf(" runtime: %lf s\n", toc - tic);
   printf("------------------------------------\n");
 
-  output_image(OUTPUT_FILE, nx, ny, image);
+  output_image(OUTPUT_FILE, nx, ny, height, image);
   free(image);
 }
 
-void stencil(const int nx, const int ny, double *  image, double *  tmp_image) {
+void stencil(const int nx, const int ny, const int height, double* image,
+             double* tmp_image)
+{
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < nx + 1; ++i) {
-      tmp_image[j+i*ny] =  image[j  +i    *ny] * 3.0/5.0;
-      tmp_image[j+i*ny] += image[j  +(i-1)*ny] * 0.5/5.0;
-      tmp_image[j+i*ny] += image[j  +(i+1)*ny] * 0.5/5.0;
-      tmp_image[j+i*ny] += image[j-1+i    *ny] * 0.5/5.0;
-      tmp_image[j+i*ny] += image[j+1+i    *ny] * 0.5/5.0;
+      tmp_image[j + i * height] =  image[j     + i       * height] * 3.0 / 5.0;
+      tmp_image[j + i * height] += image[j     + (i - 1) * height] * 0.5 / 5.0;
+      tmp_image[j + i * height] += image[j     + (i + 1) * height] * 0.5 / 5.0;
+      tmp_image[j + i * height] += image[j - 1 + i       * height] * 0.5 / 5.0;
+      tmp_image[j + i * height] += image[j + 1 + i       * height] * 0.5 / 5.0;
     }
   }
 }
 
 // Create the input image
-void init_image(const int nx, const int ny, double *  image, double *  tmp_image) {
+void init_image(const int nx, const int ny, const int height, double* image,
+                double* tmp_image)
+{
   // Zero everything
   for (int j = 0; j < ny + 2; ++j) {
     for (int i = 0; i < nx + 2; ++i) {
-      image[j+i*ny] = 0.0;
-      tmp_image[j+i*ny] = 0.0;
+      image[j + i * height] = 0.0;
+      tmp_image[j + i * height] = 0.0;
     }
   }
 
   const int tile_size = 64;
-
+  // checkerboard pattern
   for (int jb = 0; jb < ny; jb += tile_size) {
     for (int ib = 0; ib < nx; ib += tile_size) {
       if ((ib + jb) % (tile_size * 2)) {
@@ -80,7 +91,7 @@ void init_image(const int nx, const int ny, double *  image, double *  tmp_image
         const int ilim = (ib + tile_size > nx) ? nx : ib + tile_size;
         for (int j = jb + 1; j < jlim + 1; ++j) {
           for (int i = ib + 1; i < ilim + 1; ++i) {
-            image[j + i * ny] = 100.0;
+            image[j + i * height] = 100.0;
           }
         }
       }
@@ -89,10 +100,11 @@ void init_image(const int nx, const int ny, double *  image, double *  tmp_image
 }
 
 // Routine to output the image in Netpbm grayscale binary image format
-void output_image(const char * file_name, const int nx, const int ny, double *image) {
-
+void output_image(const char* file_name, const int nx, const int ny,
+                  const int height, double* image)
+{
   // Open output file
-  FILE *fp = fopen(file_name, "w");
+  FILE* fp = fopen(file_name, "w");
   if (!fp) {
     fprintf(stderr, "Error: Could not open %s\n", OUTPUT_FILE);
     exit(EXIT_FAILURE);
@@ -107,26 +119,25 @@ void output_image(const char * file_name, const int nx, const int ny, double *im
   double maximum = 0.0;
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < nx + 1; ++i) {
-      if (image[j+i*ny] > maximum)
-        maximum = image[j+i*ny];
+      if (image[j + i * height] > maximum) maximum = image[j + i * height];
     }
   }
 
   // Output image, converting to numbers 0-255
   for (int j = 1; j < ny + 1; ++j) {
     for (int i = 1; i < nx + 1; ++i) {
-      fputc((char)(255.0*image[j+i*ny]/maximum), fp);
+      fputc((char)(255.0 * image[j + i * height] / maximum), fp);
     }
   }
 
   // Close the file
   fclose(fp);
-
 }
 
 // Get the current time in seconds since the Epoch
-double wtime(void) {
+double wtime(void)
+{
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  return tv.tv_sec + tv.tv_usec*1e-6;
+  return tv.tv_sec + tv.tv_usec * 1e-6;
 }
